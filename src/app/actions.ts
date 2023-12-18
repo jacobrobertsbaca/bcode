7a7b121156efb611b1c8c5bb32f481d9faaf0199
JTRB <jacobrobertsbaca@gmail.com>
2023-12-18 12:04:36 -0700

    chore: test error throwing in server action

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -5,7 +5,7 @@ import { revalidatePath } from "next/cache";
 
 function failure(httpCode: number, message: string) {
   return {
-    result: null,
+    data: null,
     error: {
       status: httpCode,
       message,
@@ -15,7 +15,7 @@ function failure(httpCode: number, message: string) {
 
 function success<T>(result?: T) {
   return {
-    result: result ?? "Success",
+    data: result ?? "Success",
     error: null,
   };
 }
@@ -25,20 +25,26 @@ export async function deleteRoom(code: string) {
 
   /* Ensure user is logged in */
   const owner = (await supabase.auth.getUser()).data?.user?.id;
-  if (!owner) return failure(404, "Unauthorized");
+  if (!owner) return failure(401, "Unauthorized");
 
+  throw new Error("hahah");
+  
   /* Verify that room owned by this user exists */
-  let { data, error } = await supabase.from("rooms").select("code").eq("owner", owner).eq("code", code).maybeSingle();
-  if (error) return failure(500, "Error retrieving room");
+  let { data } = await supabase
+    .from("rooms")
+    .select("code")
+    .eq("owner", owner)
+    .eq("code", code)
+    .maybeSingle()
+    .throwOnError();
   if (!data) return success();
 
-  /* Delete the associated room row and code */
-  const [{ error: roomError }, { error: codeError }] = await Promise.all([
-    supabase.from("rooms").delete().eq("code", code),
-    supabase.from("diffs").delete().like("channel", `${code}-_`),
-  ]);
 
-  if (roomError || codeError) return failure(500, "Error deleting room data");
+  /* Delete the associated room row and code */
+  await Promise.all([
+    supabase.from("rooms").delete().eq("code", code).throwOnError(),
+    supabase.from("diffs").delete().like("channel", `${code}-_`).throwOnError(),
+  ]);
 
   revalidatePath("/rooms");
   return success();
