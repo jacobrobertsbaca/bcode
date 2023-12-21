2585599548c9ec9f368ca6cf5e9fb37c22de65cd
JTRB <jacobrobertsbaca@gmail.com>
2023-12-20 23:41:46 -0700

    doc: document create server client

--- src/provider/server.ts
+++ src/provider/server.ts
@@ -1,6 +1,13 @@
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
+/**
+ * Creates a Supabase server client using the service role API key.
+ * @param useCookies If `true`, will use request cookies.
+ * If a user session is present in cookies, then the client will **still** obey Supabase RLS.
+ * If `false`, cookies are ignored.
+ * @returns A Supabase client.
+ */
 export default function createServer(useCookies: boolean = false) {
   const store = cookies();
   return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVER_KEY!, {
